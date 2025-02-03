d24a9d6040dbea0f023b7bd27c95f26a0b0d3339
Tayon <tayondw@gmail.com>
2025-02-03 09:38:33 -0500

    add shadowing notes and how to use them

--- 2-variables/mutables/src/main.rs
+++ 2-variables/mutables/src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     let mut message = String::from("Name: Alfredo, Height: ");
     message.clear(); // turning into an empty string
     let mut height = 190; // must use mut because we are changing the value, if you don't use mut, you will get an error
-    height = 189; // if used let, then it is shadowing, not changing the value
+    height = 189; // this is also shadowing
     println!("{}{}", message, height);
 }
--- 4-shadowing/shadowing/src/main.rs
+++ 4-shadowing/shadowing/src/main.rs
@@ -1,7 +1,12 @@
+// Shadowing is the ability to declare a variable with the same name as another variable in the same scope.
+// Can used the previous variable to calculate the new variable.
+
 fn main() {
     let mut height = 190;
     height = height - 20;
-    let result = if height < 180 { "Tall" } else if height > 170 { "Average" } else { "Short" };
+    let result = if height < 180 { "Tall" } else if height > 170 { "Average" } else { "Short" }; 
+    // dropping the semicolons to return the value
+    // declaring the variable result and assigning it to the if statement
 
     println!("Result: {}", result);
 
