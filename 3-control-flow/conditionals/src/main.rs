2f1e0773c43a1f557bcf3d040a2df515e7a35ae9
Tayon <tayondw@gmail.com>
2025-02-03 09:23:49 -0500

    updated notes on conditions - how to use if, else if, and else statements

--- 3-control-flow/conditionals/src/main.rs
+++ 3-control-flow/conditionals/src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
-    let proceed = false;
-    if proceed {
+    let proceed = false; // can assign booleans to variables
+    if proceed { // if statement - if true then first block of code will run if not then the second block of code will run
         println!("Proceeding");
     } else {
         println!("Not proceeding");
-    }
+    } // need semicolon to end the statement so the compiler knows where the statement ends in the if and else blocks
 
-    let height = 190;
-    if height < 180 {
+    let height = 190; // declaring the height variable
+    if height < 180 { // if height is less than 180 then print "Tall"
         println!("Tall");
-    } else if height > 170 {
+    } else if height > 170 { // if height is greater than 170 then print "Average"
         println!("Average");
-    } else {
+    } else { // if height is not less than 180 or greater than 170 then print "Short"
         println!("Short");
     }
 }
