676b697db78daa842f827f9524163d40e7d07001
Tayon <tayondw@gmail.com>
2025-02-03 09:17:23 -0500

    update notes on mutable strings and how to use them

--- 2-variables/mutables/src/main.rs
+++ 2-variables/mutables/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut message = String::from("Name: Alfredo, Height: ");
-    message.clear();
-    let mut height = 190;
-    height = 189;
+    message.clear(); // turning into an empty string
+    let mut height = 190; // must use mut because we are changing the value, if you don't use mut, you will get an error
+    height = 189; // if used let, then it is shadowing, not changing the value
     println!("{}{}", message, height);
 }
