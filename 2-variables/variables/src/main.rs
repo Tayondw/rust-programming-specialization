963ac1bdc52bd8399e754a027a511c40a783d1c9
Tayon <tayondw@gmail.com>
2025-02-03 09:11:35 -0500

    added notes to variables - floats alike

--- 2-variables/variables/src/main.rs
+++ 2-variables/variables/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let message = "Name: Alfredo, Weight: ";
-    let weight = 190.0;
+    let weight = 190.0; // has to be a float to divide by 2.2, cannot be 190
 
     let kilos = weight / 2.2;
     println!("{}{}", message, weight);
