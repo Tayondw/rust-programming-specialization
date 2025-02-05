202b0c33ce976872421859d5dff1ad9ff801d5a2
Tayon <tayondw@gmail.com>
2025-02-05 22:45:50 +0000

    for loop notes updated

--- 8-for-loops/for_loops/src/main.rs
+++ 8-for-loops/for_loops/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    // the for loop using a range. Note you can use also `(1..10)` or `(1..=10)`
+    // the for loop using a range. Note you can use also `(1..10)` or `(1..=10)` 1..10 is exclusive of the last digit, 1..=10 is inclusive of the last digit
     // for i in 1..=10 {
     //     println!("i = {}", i);
     // }
 
-    // for i in (1..=5).rev() {
+    // for i in (1..=5).rev() { // reverse the order of printing
     //     println!("{}", i);
     // }
 
     let numbers = vec![1, 2, 3, 4, 5];
-    for n in numbers {
+    for n in numbers { // iterating over a collection in this case a vector - similar to array in javascript
         println!("{}", n);
     }
 }
