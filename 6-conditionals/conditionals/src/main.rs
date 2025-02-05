2c87619116df2bac3292f2d58859d0b2e5ebb686
Tayon <tayondw@gmail.com>
2025-02-05 22:35:27 +0000

    added some conditional notes

--- 6-conditionals/conditionals/src/main.rs
+++ 6-conditionals/conditionals/src/main.rs
@@ -3,8 +3,8 @@
 fn main() {
     let maybe_number: Option<Option<()>> = None;
     //let maybe_number = Some(42);
-    if let Some(number) = maybe_number {
-        println!("The number is {:?}", number);
+    if let Some(number) = maybe_number { // maybe_number will be passed in as the number in Some
+        println!("The number is {:?}", number); // use {:?} when you want to print functions or large data structures or if result is undef in other languages
     } else {
         println!("There is no number");
     }
