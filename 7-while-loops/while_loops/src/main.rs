77c403a3736588aa57b6ab5603bc283a29c90570
Tayon <tayondw@gmail.com>
2025-02-05 22:41:56 +0000

    while loop notes

--- 7-while-loops/while_loops/src/main.rs
+++ 7-while-loops/while_loops/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut i = 0;
-    while i < 5 {
+    while i < 5 { // while this condition is met
         println!("i = {}", i);
-        i += 1;
+        i += 1; // shadowing
     }
 }
